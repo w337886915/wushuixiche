a94a7afd0f31159094d83d0845ae7891c59b8504
w337886915 <wangheng19851019@126.com>
2019-01-28 10:37:50 +0800

    更新wechat

--- app/Wechat.php
+++ app/Wechat.php
@@ -2,6 +2,8 @@
 
 namespace App;
 
+
+
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Laravel\Passport\HasApiTokens;
@@ -23,13 +25,12 @@ class Wechat extends Authenticatable
     }
     //获取acctoken
     public function getAccessToken($code){
-        $url = 'https://api.weixin.qq.com/sns/jscode2session?appid=yourAppid&secret=appSecret&js_code=' . $code . '&grant_type=authorization_code';
+        $url = 'https://api.weixin.qq.com/sns/jscode2session?appid=wx4ff2f7cd3f7243f1&secret=48a1192f1afa54cfb9c2cf3e86a48833&js_code=' . $code . '&grant_type=authorization_code';
         //yourAppid为开发者appid.appSecret为开发者的appsecret,都可以从微信公众平台获取；
         $info = file_get_contents($url);//发送HTTPs请求并获取返回的数据，推荐使用curl
         $json = json_decode($info);//对json数据解码
         $arr = get_object_vars($json);
-        var_dump($arr);
-
+        dd($arr);
     }
 
 
